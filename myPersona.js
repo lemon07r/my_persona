90e67681dd88ba9cdd261385651416e46e22dec8
lemon07r <lemon07r@gmail.com>
2018-09-16 18:09:38 -0400

    Edit Button Added
    JS function to set contenteditable attribute to true for the input class elements.

---
+++ myPersona.js
@@ -0,0 +1,5 @@
+function edit() {
+    document.getElementsByClassName("input")[0].setAttribute("contenteditable", "true");
+    document.getElementsByClassName("input")[1].setAttribute("contenteditable", "true");
+    document.getElementsByClassName("input")[2].setAttribute("contenteditable", "true");
+}
